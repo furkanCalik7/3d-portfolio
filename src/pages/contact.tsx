55b9a734f06ea838941b25e1df5885f14d2404d6
Furkan <furkan.calik@ug.bilkent.edu.tr>
2025-02-01 23:39:43 +0300

    Contact page layout is improved.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -20,7 +20,8 @@ const ContactPage = () => {
         maxW="100%"
         w="100%"
         mx="auto"
-        p={{ base: 10, md: 6 }}
+        my={{ base: 0, md: 6 }}
+        p={{ base: 0, md: 10 }}
         borderRadius="lg"
         bg="rgba(0, 0, 0, 0.6)"
         backdropFilter="blur(10px)"
@@ -29,33 +30,48 @@ const ContactPage = () => {
         alignItems="center"
         flexDirection="column"
         gap={{ base: 4, md: 8 }}
+        mr="8"
       >
         {/* Contact Information Section */}
-        <Flex w="100%" direction="column" textAlign="center">
+        <Flex
+          w="100%"
+          direction="column"
+          textAlign="center"
+          mx={{ base: "0", md: "100px" }}
+        >
           <Heading as="h1" fontSize="2xl" fontWeight="bold" color="white">
             Get in Touch
           </Heading>
-          <Text fontSize="md" color="gray.300" mt={2}>
+          <Text fontSize="md" color="gray.300" mt={2} textAlign={"center"}>
             Feel free to send me a message regarding anything that might pique
-            your interest. I'll try my best to respond within a day.
+            your interest.
+          </Text>
+
+          <Text fontSize="md" color="gray.300" mt={2} textAlign={"center"}>
+            I'll try my best to respond within a day.
           </Text>
           {/* Contact Info */}
           <HStack justify="center" gap={5} mt={5}>
             <HStack>
-              <MdEmail color="#1970F1" size="20px" />
+              <MdEmail color="#ffffff" size="20px" />
               <Text color="gray.300">furkancalikk7@gmail.com</Text>
             </HStack>
             <HStack>
-              <MdLocationOn color="#1970F1" size="20px" />
+              <MdLocationOn color="#ffffff" size="20px" />
               <Text color="gray.300">Ankara, Turkiye</Text>
             </HStack>
           </HStack>
         </Flex>
 
         {/* Contact Form */}
-        <Box bg="white" borderRadius="lg" p={6} w={{ base: "100%", md: "70%" }}>
+        <Box
+          bg="gray.900"
+          borderRadius="lg"
+          p={6}
+          w={{ base: "100%", md: "70%" }}
+        >
           <VStack gap={4} w="100%">
-            <FormControl w="100%" isRequired>
+            <FormControl w="100%">
               <FormLabel mb="10">Your Name</FormLabel>
               <Input
                 type="text"
@@ -79,11 +95,17 @@ const ContactPage = () => {
                 placeholder="Write your message here..."
                 borderColor="gray.300"
                 size="md"
-                minH="150px" // Ensures it starts at 150px and expands if needed
+                minH="150px"
               />
             </FormControl>
 
-            <Button colorScheme="blue" width="full">
+            <Button
+              variant="outline"
+              width="full"
+              color="white"
+              borderColor="white" // Change this to any color that contrasts well
+              _hover={{ bg: "white", borderColor: "white", color: "black" }} // Add hover effect
+            >
               Send Message
             </Button>
           </VStack>
