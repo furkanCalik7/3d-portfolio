9901a48736787d6365c9ff2025f808e60bd82a0a
Furkan <furkan.calik@ug.bilkent.edu.tr>
2025-02-02 21:29:49 +0300

    base name is removed.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "production" ? "/3d-portfolio/" : "/",
 }));
